bf8b43801021f0de526d5893c231d85443f2b141
Brian Haas <brianjohnhaas@gmail.com>
2018-03-04 20:47:29 -0500

    works

--- bash_practice/process_spawning/spawner.sh
+++ bash_practice/process_spawning/spawner.sh
@@ -1,31 +1,46 @@
 #!/bin/bash
 
-set -e
+set -ex
+
+num_running=0
 
 get_num_running_procs() {
-    num_processes = `ps auxww | grep sleeper | wc -l`
+    num_running=`ps auxww | grep sleeper | wc -l`
 
-    return $num_processes
+    echo "num running procs: $num_running"
 }
 
 spawn_cmd() {
-    cmd=$1
+    echo "spawn_cmd()"
     
-    while [ $( get_num_running_procs ) -gt `nproc` ]; do
+    cmd=$1
+
+    get_num_running_procs
+    
+    while [ $num_running -gt `nproc` ]
+    do
         sleep 60
-        
+        get_num_running_procs
     done
 
+    echo "spawning CMD: $cmd"
     bash $cmd &
 }              
 
 main() {
-
+    echo 'main()'
+    
     for s in 1 2 3 4 5
     do
-        ./sleeper.sh $s
+        echo "Spawning $s"
+        spawn_cmd "./sleeper.sh $s"
     done
 
+    wait
+    
+    echo "done"
+    
+    exit 0
 }
 
 
